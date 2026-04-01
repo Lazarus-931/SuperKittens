f37cb5b07549a207109aef36596963d7c32c7279
Alazar Manakelew <alazermanakelewb@gmail.com>
2026-03-31 23:10:45 -0700

    Xcode Setup

---
+++ SuperKittens/MetalImpl.cpp
@@ -0,0 +1,13 @@
+//
+//  MetalImpl.c++
+//  SuperKittens
+//
+//  Created by Alazar Manakelew on 3/31/26.
+//
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include "../metal-cpp/Foundation/Foundation.hpp"
+#include "../metal-cpp/Metal/Metal.hpp"
+#include "../metal-cpp/QuartzCore/QuartzCore.hpp"
