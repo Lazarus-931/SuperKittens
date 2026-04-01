09990cd3ef860fccc38e50f6d01ffa8a5e42aee2
Alazar Manakelew <alazermanakelewb@gmail.com>
2026-03-31 22:02:58 -0700

    Initial Commit

---
+++ SuperKittens/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SuperKittens
+//
+//  Created by Alazar Manakelew on 3/31/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
